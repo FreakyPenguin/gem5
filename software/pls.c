fd037d1dc568a10450edc3091131b9ed2b59ab92
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-12-14 21:59:59 -0800

    add dummy flexnic configuration

---
+++ software/pls.c
@@ -0,0 +1,19 @@
+void pipeline_rx(void *pkt, void *meta)
+{
+  (void) pkt; (void) meta;
+}
+
+void pipeline_tx(void *pkt, void *meta)
+{
+  (void) pkt; (void) meta;
+}
+
+void pipeline_db(void *pkt, void *meta)
+{
+  (void) pkt; (void) meta;
+}
+
+void pipeline_dma(void *pkt, void *meta)
+{
+  (void) pkt; (void) meta;
+}
